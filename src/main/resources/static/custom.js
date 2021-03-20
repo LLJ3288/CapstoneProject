2a43f7919b04f3d3758e9271277167154b6ff244
LLJ3288 <74459081+LLJ3288@users.noreply.github.com>
2021-03-20 10:53:19 -0500

    Markers added

--- src/main/resources/static/custom.js
+++ src/main/resources/static/custom.js
@@ -11,41 +11,250 @@ $(document).ready(function() {
 })
 
 
+// works, leave as is
+
+//  var map;
+//     function initMap() {
+//      map = new google.maps.Map(document.getElementById('map'), {
+//        center: coords,
+//        zoom: 10,
+//        scrollwheel: false
+//      });
+//
+//      let image = {
+//              url: '/gatortwo.png',
+//              scaledSize: new google.maps.Size(75, 75)
+//        };
+//
+//      let marker = new google.maps.Marker({
+//              position: coords,
+//              map: map,
+//              icon: image,
+//              animation: google.maps.Animation.BOUNCE
+//            });
+//
+//          let contentString = '<h2>' + city + ', ' + state + ', the best place to fish in North Louisiana!</h2>';
+//
+//      let infoWindow = new google.maps.InfoWindow({
+//          content: contentString
+//      })
+//
+//      google.maps.event.addListener(marker, 'click', () => infoWindow.open(map, marker));
+//
+//    }
+
+// above working code stops here
+
+// below code starts here
 
 
 
-var map;
-//  let coords = {lat: 39.9612, lng: -82.9988}
-  function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: coords,
-      zoom: 10,
-      scrollwheel: false
-    });
 
-//    let image = {
-//            url: '/cheems.png',
-//            scaledSize: new google.maps.Size(50, 50)
-//      };
+// caddo lake starts here, works
+//
+//        var map2;
+//
+//            function initMap() {
+//              map = new google.maps.Map(document.getElementById('map'), {
+//                center: coords3,
+//                zoom: 10,
+//                scrollwheel: false
+//              });
+//
+//              let image = {
+//                      url: '/gatortwo.png',
+//                      scaledSize: new google.maps.Size(75, 75)
+//                };
+//
+//              let marker = new google.maps.Marker({
+//                      position: coords3,
+//                      map: map,
+//                      icon: image,
+//                      animation: google.maps.Animation.BOUNCE
+//                    });
+//
+//
+//                  let contentString2 = '<h2>The best place to fish in North Louisiana!</h2>';
+//
+//
+//
+//
+//              let infoWindow = new google.maps.InfoWindow({
+//                  content: contentString2
+//              })
+//
+//              google.maps.event.addListener(marker, 'click', () => infoWindow.open(map, marker));
+//
+//            }
+//
+// above code stops here
 
-    let marker = new google.maps.Marker({
-            position: coords,
-            map: map,
-//            icon: image,
-//            animation: google.maps.Animation.BOUNCE
-          });
 
-   // let contentString = `<h2> ${city}, ${state} </h2>`;
-   // let contentString = '<h2>' + city + ', ' + state + ', where your dreams come true!</h2>';
-        let contentString = '<h2>' + city + ', ' + state + ', the best place to live in the world!</h2>';
+// this block starts here, 3
+//
+        var map2;
+
+                    function initMap() {
+                      map = new google.maps.Map(document.getElementById('map'), {
+                        center: coords3,
+                        zoom: 10,
+                        scrollwheel: true
+                      });
+
+                      let image = {
+                              url: '/gatortwo.png',
+                              scaledSize: new google.maps.Size(75, 75)
+                        };
+
+                      let marker = new google.maps.Marker({
+                              position: coords3,
+                              map: map,
+                              icon: image,
+                              animation: google.maps.Animation.BOUNCE
+                            });
+
+                       let marker2 = new google.maps.Marker({
+                                                    position: coords,
+                                                    map: map,
+                                                    icon: image,
+                                                    animation: google.maps.Animation.BOUNCE
+                                                  });
+
+                       let marker3 = new google.maps.Marker({
+                                                     position: coords4,
+                                                     map: map,
+                                                     icon: image,
+                                                     animation: google.maps.Animation.BOUNCE
+                                                   });
+
+//                              let contentString2 = '<h2>The best place to fish in North Louisiana!</h2>';
+//
+//                                                   let infoWindow = new google.maps.InfoWindow({
+//                                                       content: contentString2
+//                                                   })
+//                                                   google.maps.event.addListener(marker, 'click', () => infoWindow.open(map, marker));
+//
+////                                                 }
+                        let marker5 = new google.maps.Marker({
+                                                      position: coords6,
+                                                      map: map,
+                                                      icon: image,
+                                                      animation: google.maps.Animation.BOUNCE
+                                                    });
+
+                        let marker6 = new google.maps.Marker({
+                                                      position: coords7,
+                                                      map: map,
+                                                      icon: image,
+                                                      animation: google.maps.Animation.BOUNCE
+                                                    });
+
+
+                        let marker7 = new google.maps.Marker({
+                                                      position: coords8,
+                                                      map: map,
+                                                      icon: image,
+                                                      animation: google.maps.Animation.BOUNCE
+                                                    });
+
+
+
+                       let marker4 = new google.maps.Marker({
+                                                     position: coords5,
+                                                     map: map,
+                                                     icon: image,
+                                                     animation: google.maps.Animation.BOUNCE
+                                                                          });
+
+                      let contentString = '<h2>The best place to fish in North Louisiana!</h2>';
+
+                      let infoWindow = new google.maps.InfoWindow({
+                          content: contentString2
+                      })
+                      google.maps.event.addListener(marker, 'click', () => infoWindow.open(map, marker));
+
+                    }
+//
+// this block stops here, 3
 
 
 
 
-    let infoWindow = new google.maps.InfoWindow({
-        content: contentString
-    })
 
-    google.maps.event.addListener(marker, 'click', () => infoWindow.open(map, marker));
 
-  }
+
+//  var map2;
+//    let coords = {lat: 32.5221828, lng: -93.7651944}
+//    function initMap() {
+//      map = new google.maps.Map(document.getElementById('map'), {
+//        center: coords,
+//        zoom: 10,
+//        scrollwheel: false
+//      });
+//
+//  //    let image = {
+//  //            url: '/cheems.png',
+//  //            scaledSize: new google.maps.Size(50, 50)
+//  //      };
+//
+//      let marker = new google.maps.Marker({
+//              position: coords,
+//              map: map,
+//  //            icon: image,
+//  //            animation: google.maps.Animation.BOUNCE
+//            });
+//
+//     // let contentString = `<h2> ${city}, ${state} </h2>`;
+//     // let contentString = '<h2>' + city + ', ' + state + ', where your dreams come true!</h2>';
+//          let contentString2 = '<h2>' ' the 2nd best place to live in the world!</h2>';
+//
+//
+//
+//
+//      let infoWindow = new google.maps.InfoWindow({
+//          content: contentString2
+//      })
+//
+//      google.maps.event.addListener(marker, 'click', () => infoWindow.open(map, marker));
+//
+//    }
+//
+
+
+
+//  original
+//  var map;
+//  //  let coords = {lat: 39.9612, lng: -82.9988}
+//    function initMap() {
+//      map = new google.maps.Map(document.getElementById('map'), {
+//        center: coords,
+//        zoom: 10,
+//        scrollwheel: false
+//      });
+//
+//  //    let image = {
+//  //            url: '/cheems.png',
+//  //            scaledSize: new google.maps.Size(50, 50)
+//  //      };
+//
+//      let marker = new google.maps.Marker({
+//              position: coords,
+//              map: map,
+//  //            icon: image,
+//  //            animation: google.maps.Animation.BOUNCE
+//            });
+//
+//     // let contentString = `<h2> ${city}, ${state} </h2>`;
+//     // let contentString = '<h2>' + city + ', ' + state + ', where your dreams come true!</h2>';
+//          let contentString = '<h2>' + city + ', ' + state + ', the best place to live in the world!</h2>';
+//
+//
+//
+//
+//      let infoWindow = new google.maps.InfoWindow({
+//          content: contentString
+//      })
+//
+//      google.maps.event.addListener(marker, 'click', () => infoWindow.open(map, marker));
+//
+//    }
